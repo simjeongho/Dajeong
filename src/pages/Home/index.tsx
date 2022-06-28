6dbb0ad375ca49c8d5419e7a5b6906eeb4a77fda
simjeongho <simjeongho012@gmail.com>
2022-06-28 23:35:57 +0900

    refactor: 홈 디자인 수정

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,18 @@
-import AxiosTestComponent from "@components/Test";
-import React from "react";
+import HomeImageCard from "@components/HomeImageCard";
 import styled from "styled-components";
-
-const HomeContainer = styled.div``;
+import HomeBackground from "@assets/image/home.jpg";
+const HomeContainer = styled.div`
+	width: 100%;
+	height: 80%;
+	background-color: var(--color-lightgray);
+	background-image: url(HomeBackground);
+`;
 
 const Home = () => {
 	return (
 		<HomeContainer>
-			<AxiosTestComponent />
+			<HomeImageCard />
+			<div>왜 안돼</div>
 		</HomeContainer>
 	);
 };
