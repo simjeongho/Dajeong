29edefd654ae1a544e6b43925a503710c4a0cba2
simjeongho <simjeongho012@gmail.com>
2022-06-28 19:15:20 +0900

    fix: 레이아웃 수정

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -1,16 +1,24 @@
-import React, { ReactElement } from "react";
+import React from "react";
 import Header from "./Header";
 import styled from "styled-components";
+import Footer from "./Footer";
 
 const LayoutContainer = styled.div`
 	display: flex;
+	flex-direction: column;
+	width: 100%;
+	height: 100%;
+	@media screen and (max-width: 768px) {
+		padding: 0;
+	}
 `;
 
-const Layout = (props: { children: ReactElement }) => {
+const Layout = (props: { children: React.ReactNode }) => {
 	return (
 		<LayoutContainer>
 			<Header />
 			<main>{props.children}</main>
+			<Footer />
 		</LayoutContainer>
 	);
 };
