62868f3481f59a41a133de05c754dd61441b60b7
simjeongho <simjeongho012@gmail.com>
2022-06-28 19:17:20 +0900

    feat: 레이아웃 footer생성

---
+++ src/Layout/Footer/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { LayoutFooter } from "./styled";
+
+const Footer = () => {
+	return <LayoutFooter></LayoutFooter>;
+};
+
+export default Footer;
---
+++ src/Layout/Footer/styled.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const LayoutFooter = styled.footer`
+	width: 100%;
+	height: 30%;
+	background: black;
+	border-radius: 8px;
+	position: relative;
+`;
