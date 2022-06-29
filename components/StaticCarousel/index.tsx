847577199291234fdd40c02acb36a6cef953d0c5
simjeongho <simjeongho012@gmail.com>
2022-06-29 19:35:24 +0900

    fix: 이미지 크기 다른 사진 삭제

--- components/StaticCarousel/index.tsx
+++ components/StaticCarousel/index.tsx
@@ -5,15 +5,14 @@ import { MdArrowBackIos, MdArrowForwardIos } from "react-icons/md";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Card from "components/card";
-import mangWon from "assets/image/mangwon.jpg";
-import hanRiver from "assets/image/hanriver.jpg";
-import gangneungdaeun from "assets/image/gangneungdaeun.jpg";
-import incheonpark from "assets/image/incheonpark.jpg";
-import inhoo from "assets/image/inhoo.jpg";
-import construct from "assets/image/construct.jpg";
-import daebudo from "assets/image/daebudo.jpg";
-import moouido from "assets/image/moouido.jpg";
-import prettydaeun from "assets/image/prettydaeun.jpg";
+import mangWon from "assets/images/mangwon.jpg";
+import hanRiver from "assets/images/hanriver.jpg";
+import gangneungdaeun from "assets/images/gangneungdaeun.jpg";
+import incheonpark from "assets/images/incheonpark.jpg";
+import inhoo from "assets/images/inhoo.jpg";
+import daebudo from "assets/images/daebudo.jpg";
+import moouido from "assets/images/moouido.jpg";
+import prettydaeun from "assets/images/prettydaeun.jpg";
 
 interface Props {
 	settings?: Settings;
@@ -75,13 +74,10 @@ const StaticSlider: React.FC<Props> = ({ settings = DEFAULT_SETTINGS }) => (
 	<ReactSlick {...settings}>
 		<Card src={mangWon} alt="망원" title="망원에서" year="2022" />
 		<Card src={hanRiver} alt="한강" title="한강 피크닉" year="2022" />
-		<Card src={gangneungdaeun} alt="강릉에서 드라이브" title="강릉에서 드라이브" year="2021" />
 		<Card src={incheonpark} alt="인천대공원" title="인천대공원" year="2022" />
 		<Card src={inhoo} alt="인하대 후문" title="인하대 후문" year="2022" />
-		<Card src={construct} alt="집 풍경" title="집 풍경" year="2022" />
 		<Card src={daebudo} alt="첫 대부도" title="첫 대부도" year="2022" />
 		<Card src={moouido} alt="무의도" title="무의도" year="2022" />
-		<Card src={prettydaeun} alt="예쁜 다은" title="이태원 나들이" year="2022" />
 	</ReactSlick>
 );
 
