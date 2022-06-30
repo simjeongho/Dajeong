4380af68332837398d0ea3ee114a9f55f5a7088e
simjeongho <simjeongho012@gmail.com>
2022-07-01 00:55:24 +0900

    feat: writingPosts부분

---
+++ pages/writingposts/index.tsx
@@ -0,0 +1,8 @@
+import { Background } from "shared/component/styled";
+import { CustomQuillEditor } from "./styled";
+
+const WritingPostsPage = () => {
+	return <CustomQuillEditor />;
+};
+
+export default WritingPostsPage;
---
+++ pages/writingposts/styled.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+import QuillEditor from "components/Quilleditor";
+export const CustomQuillEditor = styled(QuillEditor)`
+	height: 50vh;
+	width: 80vw;
+	margin: 5%;
+`;
