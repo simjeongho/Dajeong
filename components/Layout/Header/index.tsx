a023c85e3f7f5a6fc30f6e5d1aa5fc0586b8cebd
simjeongho <simjeongho012@gmail.com>
2022-06-30 14:58:09 +0900

    fix: 헤더 디자인 수정

--- components/Layout/Header/index.tsx
+++ components/Layout/Header/index.tsx
@@ -1,19 +1,12 @@
-import {
-	LayoutHeader,
-	LayoutHeaderImg,
-	LayoutHeaderTitle,
-	LayoutNavbar,
-	LayoutNavItem,
-	LayoutNavLists,
-	LayoutTitlediv,
-} from "./styled";
-import GinsenTitle from "assets/image/ginsen.svg";
+import { LayoutHeader, LayoutHeaderTitle, LayoutNavbar, LayoutNavItem, LayoutNavLists, LayoutTitlediv } from "./styled";
+import GinsenTitle from "assets/images/ginsen.svg";
+import Image from "next/image";
 const Header = () => {
 	return (
 		<LayoutHeader>
 			<LayoutTitlediv>
 				<LayoutHeaderTitle>심 봤 다 Blog</LayoutHeaderTitle>
-				<LayoutHeaderImg src={GinsenTitle} alt="ginsen" />
+				<Image src={GinsenTitle} alt="Ginsen" height={100} />
 			</LayoutTitlediv>
 
 			<LayoutNavbar>
--- components/Layout/Header/styled.ts
+++ components/Layout/Header/styled.ts
@@ -3,21 +3,19 @@ import styled from "styled-components";
 export const LayoutHeader = styled.header`
 	width: 100%;
 	height: auto;
-	position: sticky;
 	background-color: black;
 	border-radius: 10px;
 `;
 
 export const LayoutTitlediv = styled.div`
 	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
+	width: max-content;
+	margin: 1.5% 10%;
 `;
 
 export const LayoutHeaderTitle = styled.h1`
 	height: 100%;
-	font-family: var(--font-googleGugi);
+	font-family: "googlGugi";
 	color: white;
 	font-size: 3rem;
 	margin: 1rem;
@@ -45,9 +43,8 @@ export const LayoutNavItem = styled.li`
 	list-style: none;
 	font-size: 1.5rem;
 	color: white;
-	font-family: var(--font-googleSingleDay);
+	font-family: var(--font-lnkLi);
 	border: 2px solid transparent;
-	background-image: linear-gradient(black, black), linear-gradient(to right, red 0%, orange 100%);
 	background-origin: border-box;
 	background-clip: content-box, border-box;
 `;
