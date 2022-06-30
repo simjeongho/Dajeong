624508906218ed3b821d97a064fb62c484992309
simjeongho <simjeongho012@gmail.com>
2022-06-30 23:20:14 +0900

    design: shared 애니매이션 추가

--- shared/style/styled.ts
+++ shared/style/styled.ts
@@ -1,5 +1,17 @@
 import styled, { keyframes } from "styled-components";
 
+export const moveUptop = keyframes`
+    0%{
+        top: 1rem;
+    }
+    50%{
+        top: 0.75rem;
+    }
+    100% {
+        top: 0.5rem;
+    }
+`;
+
 export const moveUp = keyframes`
     0%{
         margin-top: 5rem;
