0fda92e8da2315f3bae92fbcf4403385f9080895
simjeongho <simjeongho012@gmail.com>
2022-07-01 01:20:57 +0900

    refactor: shared 컴포넌트 제작

---
+++ shared/component/MiddlePurpleButton/styled.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+export const MiddelPurpleButton = styled.button`
+	width: 40%;
+	height: auto;
+	font-size: 1.5rem;
+	color: white;
+	background-color: var(--color-middlepurple);
+	margin-top: 5rem;
+	border-radius: 10px 10px 10px 10px;
+	cursor: pointer;
+`;
---
+++ shared/component/styled.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Background = styled.div`
+	width: 100%;
+	height: 100%;
+	background-color: black;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+`;
