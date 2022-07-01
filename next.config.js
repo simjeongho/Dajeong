6de1625c14602ee45472b1259318d64d4f35d941
simjeongho <simjeongho012@gmail.com>
2022-07-01 14:01:17 +0900

    feat: nextjs proxy 설정

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 const withImages = require("next-images");
 module.exports = {
+	async rewrites() {
+		return [
+			{
+				source: "/:path*",
+				destination: "http://localhost:5000/:path*",
+			},
+		];
+	},
 	images: {
 		disaleStaticImages: true,
 	},
