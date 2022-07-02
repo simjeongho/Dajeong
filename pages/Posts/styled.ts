838cbab5dbfe3da01565280bc0af83ab573b8c86
simjeongho <simjeongho012@gmail.com>
2022-07-02 21:13:27 +0900

    fix: 스타일 수정

--- pages/Posts/styled.ts
+++ pages/Posts/styled.ts
@@ -1,7 +1,7 @@
-import { MiddelPurpleButton } from "./../../shared/component/MiddlePurpleButton/styled";
+import { MiddlePurpleButton } from "./../../shared/component/MiddlePurpleButton/styled";
 import styled from "styled-components";
 
-export const WritingPostsButton = styled(MiddelPurpleButton)`
+export const WritingPostsButton = styled(MiddlePurpleButton)`
 	width: 300px;
 	position: relative;
 	right: 30%;
