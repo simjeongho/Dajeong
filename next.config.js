398a77795e3ae031b83cc3dc308b52e903f996a2
simjeongho <simjeongho012@gmail.com>
2022-07-05 14:09:19 +0900

    feat: nextJS외부 이미지 불러오기 설정

--- next.config.js
+++ next.config.js
@@ -1,14 +1,27 @@
-const withImages = require("next-images");
-module.exports = {
+const nextConfig = {
 	images: {
-		domains: ["localhost"],
+		domains: ["localhost:5000", "*"],
+		remotePatterns: [
+			{
+				protocol: "http",
+				hostname: "localhost",
+				port: "5000",
+				pathname: "/account123/**",
+			},
+		],
 	},
 	async rewrites() {
 		return [
 			{
-				source: "/:path*",
-				destination: "http://localhost:5000/:path*",
+				source: `/:path*`,
+				destination: `http://localhost:5000/:path*`,
+			},
+			{
+				source: `/Album`,
+				destination: `http://localhost:5000/singleAlbum/getList`,
 			},
 		];
 	},
 };
+
+module.exports = nextConfig;
