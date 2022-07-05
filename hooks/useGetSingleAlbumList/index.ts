bd61fc6b21ae5596530bc112eca57b6a4daed06f
simjeongho <simjeongho012@gmail.com>
2022-07-06 00:22:43 +0900

    feat: react-query로 데이터 불러오기

---
+++ hooks/useGetSingleAlbumList/index.ts
@@ -0,0 +1,14 @@
+import axios, { AxiosError, AxiosResponse } from "axios";
+import { singleAlbumPost } from "pages/Album";
+import { useQuery } from "react-query";
+export type singleAlbumPostAxios = {
+	singleAlbumList: singleAlbumPost[];
+};
+
+const getSingleAlbumList = () => axios.get("http://localhost:5000/singleAlbum/getList");
+
+const useGetSingleAlbumList = () => {
+	return useQuery<AxiosResponse<singleAlbumPostAxios>, AxiosError>("getSingleAlbumList", getSingleAlbumList);
+};
+
+export default useGetSingleAlbumList;
--- pages/Album/index.tsx
+++ pages/Album/index.tsx
@@ -6,12 +6,14 @@ import { useEffect, useState } from "react";
 import { AlbumListContainer, SingleAlbumWritesButton } from "./styled";
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import { Props } from "next/script";
-type singleAlbumPost = {
+import useGetSingleAlbumList from "hooks/useGetSingleAlbumList";
+export type singleAlbumPost = {
 	title: string;
 	content: string;
 	filePath: string;
 	postNum: number;
 };
+
 // export const getServerSideProps: GetServerSideProps = async () => {
 // 	try {
 // 		const singleAlbums: singleAlbumPost[] = await axios
@@ -29,19 +31,25 @@ type singleAlbumPost = {
 // 	}
 // };
 const Albums = ({ singleAlbums }: InferGetServerSidePropsType<{ singleAlbums: singleAlbumPost[] }>) => {
-	const [singleAlbumList, setSingleAlbumList] = useState<singleAlbumPost[] | null>(null);
+	//const [singleAlbumList, setSingleAlbumList] = useState<singleAlbumPost[] | null>(null);
 	//const singleAlbumList: singleAlbumPost[] = singleAlbums;
+	// useEffect(() => {
+	// 	axios
+	// 		.get("/singleAlbum/getList")
+	// 		.then((response) => {
+	// 			console.log(response.data.singleAlbumList);
+	// 			setSingleAlbumList(response.data.singleAlbumList);
+	// 		})
+	// 		.catch((err) => {
+	// 			console.log(err);
+	// 		});
+	// }, []);
+	const { data, isLoading } = useGetSingleAlbumList();
+	const singleAlbumList = data;
 	useEffect(() => {
-		axios
-			.get("/singleAlbum/getList")
-			.then((response) => {
-				console.log(response.data.singleAlbumList);
-				setSingleAlbumList(response.data.singleAlbumList);
-			})
-			.catch((err) => {
-				console.log(err);
-			});
+		console.log("data", singleAlbumList);
 	}, []);
+
 	const handleContentLength = (content: string) => {
 		if (content.length > 15) {
 			return content.slice(0, 15) + "...";
@@ -49,27 +57,31 @@ const Albums = ({ singleAlbums }: InferGetServerSidePropsType<{ singleAlbums: si
 			return content;
 		}
 	};
-	console.log("singleAlbumList", singleAlbumList);
 	return (
 		<AlbumListContainer>
 			<Link href="Album/SingleAlbumWrites">
 				<SingleAlbumWritesButton>싱글 앨범 작성</SingleAlbumWritesButton>
 			</Link>
-			<AxiosSlider title="Single Albums">
-				{singleAlbumList?.map((postitem) => (
-					<LinkCard
-						key={postitem.filePath}
-						src={`http://localhost:5000/${postitem.filePath}`}
-						title={postitem.title}
-						alt={postitem.content}
-						year={handleContentLength(postitem.content)}
-						width={300}
-						height={400}
-						linkurl={`/Album/singleAlbum/[postNum]`}
-						query={postitem.postNum}
-					></LinkCard>
-				))}
-			</AxiosSlider>
+
+			{isLoading ? (
+				<div>is Loading...</div>
+			) : (
+				<AxiosSlider title="Single Albums">
+					{data?.data.singleAlbumList.map((postitem) => (
+						<LinkCard
+							key={postitem.filePath}
+							src={`http://localhost:5000/${postitem.filePath}`}
+							title={postitem.title}
+							alt={postitem.content}
+							year={handleContentLength(postitem.content)}
+							width={300}
+							height={400}
+							linkurl={`/Album/singleAlbum/[postNum]`}
+							query={postitem.postNum}
+						></LinkCard>
+					))}
+				</AxiosSlider>
+			)}
 		</AlbumListContainer>
 	);
 };
