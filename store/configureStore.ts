e4140deb31e0928f37c1f8aa3f23d7215db0e745
simjeongho <simjeongho012@gmail.com>
2022-07-11 15:34:04 +0900

    feat: redux-saga 연결

--- store/configureStore.ts
+++ store/configureStore.ts
@@ -1,15 +1,21 @@
 //ConfigureStore.ts
+import { Store } from "redux";
 import { createWrapper } from "next-redux-wrapper";
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "store/slices/root";
-import ThunkMiddleware from "redux-thunk";
-
+import createSagaMiddleware, { Task } from "redux-saga";
+import { rootSaga } from "./saga/root";
+export interface SagaStore extends Store {
+	sagaTask?: Task;
+}
 const createStore = () => {
+	const sagaMiddleware = createSagaMiddleware();
 	const store = configureStore({
 		reducer: rootReducer,
-		middleware: [ThunkMiddleware],
+		middleware: [sagaMiddleware],
 		devTools: process.env.NODE_ENV !== "production",
 	});
+	(store as SagaStore).sagaTask = sagaMiddleware.run(rootSaga);
 	return store;
 };
 
