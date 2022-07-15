d2c8beec171c103bbacb356cd2ef96bf130089a7
simjeongho <simjeongho012@gmail.com>
2022-07-16 02:08:40 +0900

    feat: 새로고침해도 로그인 유지

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,25 @@ import Header from "components/Layout/Header";
 import "../styles/globals.css";
 import { LayoutContainer } from "components/Layout/styled";
 import type { AppProps } from "next/app";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { QueryClient, QueryClientProvider, Hydrate } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import "antd/dist/antd.css";
 import wrapper from "store/configureStore";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { setUserData } from "store/slices/user-slice";
 function MyApp({ Component, pageProps }: AppProps) {
 	//useState lazyinit을 사용해 QueryClient 인스턴스를 생성해
 	//QueryClientProvider의 client 값으로 전달해준다.
 	const [queryClient] = useState(() => new QueryClient());
+	const dispatch = useDispatch();
+	useEffect(() => {
+		axios.get("/user", { withCredentials: true }).then((res) => {
+			console.log("홈!", res);
+			dispatch(setUserData(res.data));
+		});
+	}, []);
 	return (
 		<>
 			<QueryClientProvider client={queryClient}>
