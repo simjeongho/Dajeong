751af992cd22a10ff5d760714627046057fc2619
simjeongho <simjeongho012@gmail.com>
2022-07-19 20:33:26 +0900

    fix: footer디자인 수정

--- components/Layout/Footer/styled.ts
+++ components/Layout/Footer/styled.ts
@@ -14,11 +14,12 @@ export const LayoutFooterList = styled.ul`
 	align-items: center;
 	justify-content: center;
 	height: 100%;
+	color: white;
 `;
 
 export const LayoutFooterListItem = styled.li`
 	list-style: none;
 	font-family: var(--Font-googleSingleDay);
-	color: var(--color-text-white);
+	color: white;
 	font-size: 1rem;
 `;
